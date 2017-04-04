bf2fefa6dc03fe7836d06f5da45e9cc77f79b75f
DeedleFake <deedlefake@users.noreply.github.com>
2017-04-04 16:13:30 -0400

    cmd/signage: Generate RSS for signed bills.

--- cmd/signage/signage.go
+++ cmd/signage/signage.go
@@ -1,19 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io"
 	"log"
+	"net/http"
+	"text/template"
 
 	"github.com/DeedleFake/signage"
 )
 
-func main() {
-	entries, err := signage.GetSigned()
+var (
+	tmpl *template.Template
+)
+
+func init() {
+	tmpl = template.Must(template.New("rss").Parse(`<?xml version='1.0' encoding='UTF-8' ?>
+<rss version='2.0'>
+	<channel>
+		<title>{{ .Type }} Bills</title>
+
+		{{ range .Bills -}}
+		<item>
+			<title>{{ .Title }}</title>
+			<link>{{ .URL }}</link>
+			<pubDate>{{ .Date }}</pubDate>
+		</item>
+		{{- end }}
+	</channel>
+</rss>`))
+}
+
+func handleSigned(rw http.ResponseWriter, req *http.Request) {
+	bills, err := signage.GetSigned()
 	if err != nil {
-		log.Fatalf("Failed to scrape: %v", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	for _, e := range entries {
-		fmt.Printf("%#v\n", e)
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, map[string]interface{}{
+		"Type":  "Signed",
+		"Bills": bills,
+	})
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	_, err = io.Copy(rw, &buf)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handleSigned)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
