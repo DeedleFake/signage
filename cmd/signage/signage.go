f1cbaede488e6b380b27544d43c55ff6fbe412ee
DeedleFake <deedlefake@users.noreply.github.com>
2017-04-04 17:18:40 -0400

    cmd/signage: Add support for other modes.

--- cmd/signage/signage.go
+++ cmd/signage/signage.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"path"
+	"unicode"
 
 	"github.com/DeedleFake/signage"
 )
@@ -29,30 +30,30 @@ func marshalJSON(t string, bills []signage.Bill) (io.Reader, error) {
 	return bytes.NewReader(buf), err
 }
 
-func handleSigned(rw http.ResponseWriter, req *http.Request) {
-	mode := path.Ext(req.URL.Path)
-	if mode == "" {
-		mode = ".rss"
+func handle(rw http.ResponseWriter, req *http.Request, mode string, get billFunc) {
+	format := path.Ext(req.URL.Path)
+	if format == "" {
+		format = ".rss"
 	}
 
 	var marshal func(t string, bills []signage.Bill) (io.Reader, error)
-	switch mode {
+	switch format {
 	case ".rss":
 		marshal = marshalRSS
 	case ".json":
 		marshal = marshalJSON
 	default:
-		http.Error(rw, fmt.Sprintf("Unknown format: %q", mode), http.StatusBadRequest)
+		http.Error(rw, fmt.Sprintf("Unknown format: %q", format), http.StatusBadRequest)
 		return
 	}
 
-	bills, err := signage.GetSigned()
+	bills, err := get()
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	buf, err := marshal("Signed", bills)
+	buf, err := marshal(mode, bills)
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
@@ -65,7 +66,31 @@ func handleSigned(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
+type billFunc func() ([]signage.Bill, error)
+
+var (
+	modes = map[string]billFunc{
+		"signed":  signage.GetSigned,
+		"vetoed":  signage.GetVetoed,
+		"pending": signage.GetPending,
+	}
+)
+
+func mux(rw http.ResponseWriter, req *http.Request) {
+	name := path.Base(req.URL.Path)
+	ext := path.Ext(name)
+
+	mode := name[:len(name)-len(ext)]
+	get, ok := modes[mode]
+	if !ok {
+		http.Error(rw, fmt.Sprintf("Unknown get: %q", get), http.StatusBadRequest)
+		return
+	}
+
+	mode = string(unicode.ToUpper(rune(mode[0]))) + mode[1:]
+	handle(rw, req, mode, get)
+}
+
 func main() {
-	http.HandleFunc("/", handleSigned)
-	log.Fatalln(http.ListenAndServe(":8080", nil))
+	log.Fatalln(http.ListenAndServe(":8080", http.HandlerFunc(mux)))
 }
